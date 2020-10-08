e422393893ff015daf80ad1d9fadf2ebdab3a4df
cdrbbt <egor.bykadorov@gmail.com>
2020-10-08 15:56:05 +0300

    3.8 phonebook backend

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { response } = require('express')
 const express = require('express')
 const app = express()
-
 const morgan = require('morgan')
 
-app.use(morgan('tiny'))
-
 app.use(express.json())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 
 let persons = [
   {
