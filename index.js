54fccefdae1a3cfce066631862426a6f17759047
cdrbbt <egor.bykadorov@gmail.com>
2020-10-09 18:06:48 +0300

    3.9 phonebook backend

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
