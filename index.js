bafd283d6b99c309edcd5dfaf05d3a6657450f11
cdrbbt <egor.bykadorov@gmail.com>
2021-02-05 17:10:29 +0200

    3.18 info and id

--- index.js
+++ index.js
@@ -39,18 +39,22 @@ let persons = [
 ]
 
 const generateId = () => {
-  return Math.floor(Math.random()*10000000)
+  return Math.floor(Math.random() * 10000000)
 }
 
-app.get('/info', (req, res) => {
-  res.send(`<p>There are ${persons.length} people registered in the phonebook</p><p>As of ${new Date().toString()}</p>`)
+app.get('/info', (req, res, next) => {
+  Person.estimatedDocumentCount().then(c => {
+    res.send(`<p>There are ${c} people registered in the phonebook</p><p>As of ${new Date().toString()}</p>`)
+  }).catch(error => {
+    next({ error, msg: 'failed to count' })
+  })
 })
 
 app.get('/api/persons', (req, res) => {
-  Person.find().then(persons =>{
+  Person.find().then(persons => {
     res.json(persons)
-  }).catch( error => {
-    next({error, msg:'failed to fetch persons list'})
+  }).catch(error => {
+    next({ error, msg: 'failed to fetch persons list' })
   })
 })
 
@@ -58,54 +62,73 @@ app.post('/api/persons', (req, res) => {
   const body = req.body
 
   if (!body.name || !body.number) {
-    return res.status(400).json({error: 'missing data'})
+    return res.status(400).json({ error: 'missing data' })
   }
 
-  if (persons.find(p => p.name === body.name)) {
-    return res.status(400).json({error: 'name must be unique'})
-  }
+  Person.findOne({ name: body.name }).then(person => {
+    console.log('after searching for person')
+    console.log(person)
+    if (person) {
+      return res.status(400).json({ error: 'person alraedy in db', id: person.id })
+    }
+    const newPerson = new Person({
+      name: body.name,
+      number: body.number,
+    })
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
+    newPerson.save().then(p => {
+      res.json(p)
+    }).catch(error => {
+      next({ error, msg: 'failed to save to db' })
+    })
 
-  person.save().then(newPerson => {
-    res.json(newPerson)
-  }).catch(error => {
-    next({error, msg:'failed to save to db'})
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(p => p.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
+  Person.findById(id).then(p => {
+    if (p) {
+      res.json(p)
+    } else {
+      res.status(404).end()
+    }
+  }).catch(error => {
+    next({error, msg:'failed to get person'})
+  })
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  return
+
+  const body = req.body
+
+  if (!body.number) {
+    return res.status(400).json({ error: 'missing data' })
+  }
+
+  Person.findByIdAndUpdate(req.params.id, { number: body.number }, { new: true }).then(p =>
+    res.json(p)
+  ).catch(error => {
+    next({ error, msg: 'failed to update' })
+  })
+
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-  .then(result => {
-    res.status(204).end()
-  }).catch(error => next(error))
+    .then(result => {
+      res.status(204).end()
+    }).catch(error => next(error))
 })
 
 const errorHandler = (error, req, res, next) => {
   console.error(error)
-  res.status(500).send({error:error.msg})
+  res.status(500).send({ error: error.msg })
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`)
 })
